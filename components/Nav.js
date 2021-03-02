69476c80a72c5fe19ab4253ffbcad3edbd3ca33e
Cameron Brantley <brantley.cameron@gmail.com>
2021-03-02 15:12:11 -0600

    Working Nav Menu

--- components/Nav.js
+++ components/Nav.js
@@ -1,11 +1,10 @@
 export default () => `
-  <nav>
-    <i class="fas fa-bars"></i>
-    <ul class="hidden--mobile nav-links">
-      <li><a href="index.html">Home</a></li>
-      <li><a href="bio.html">Bio</a></li>
-      <li><a href="gallery.html">Gallery</a></li>
-      <li><a href="register.html">Register</a></li>
-    </ul>
-  </nav>
+<nav>
+  <i class="fas fa-bars"></i>
+  <ul class="hidden--mobile nav-links">
+    <li><a href="#bio">Bio</a></li>
+    <li><a href= "#gallery">Gallery</a></li>
+    <li><a href="#register">Register</a></li>
+  </ul>
+</nav>
 `;
