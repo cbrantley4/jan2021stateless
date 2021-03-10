48f29efecfb98c06f5591f2bff6d0dd21e536dc9
Cameron Brantley <brantley.cameron@gmail.com>
2021-03-09 21:10:21 -0600

    Present weather info

--- components/views/Home.js
+++ components/views/Home.js
@@ -1,8 +1,9 @@
 import html from "html-literal";
 
-export default () => html`
+export default st => html`
   <section id="jumbotron">
     <h2>Savvy Coders Jan. 2021 Cohort</h2>
     <a href="register.html">"Call to Action" "Button"</a>
+    <h3>Weather in ${st.weather.city}: ${st.weather.description}.</h3>
   </section>
 `;
