f0ad6ffc7cd957fec7b352fd713ed681cb40b6cf
Cameron Brantley <brantley.cameron@gmail.com>
2021-03-09 20:43:33 -0600

    Blog posts

--- index.js
+++ index.js
@@ -31,6 +31,10 @@ axios.get("http://jsonplaceholder.typicode.com/posts").then(response => {
   response.data.forEach(post => {
     state.Blog.posts.push(post);
   });
+  const params = router.lastRouteResolved().params;
+  if (params) {
+    render(state[params.page]);
+  }
 });
 
 // add menu toggle to bars icon in nav bar
